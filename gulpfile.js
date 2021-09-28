6b1cb230ea200ea27696e8c4fa8713fb555a52b0
Hizbullah Fuad <hizbullah.fuad@kaz.com.bd>
2021-09-28 17:12:29 +0600

    dist folder updated

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 
 gulp.task('default', function () {
     gulp.src(['**/*.*'])
-        .pipe(gulp.dest('./dist/lens'));
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch', function() {
